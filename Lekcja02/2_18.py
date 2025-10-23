ca2cea487354fb6197f49fc269603061e2cbde53
Sobi1444 <michal.kryn6@gmail.com>
2025-10-23 15:27:16 +0200

    Zadania Zestaw 2

---
+++ Lekcja02/2_18.py
@@ -0,0 +1,4 @@
+liczba = 10003243000532500
+liczba = str(liczba)
+print(liczba.count("0"))
+
---
+++ Lekcja02/2_19.py
@@ -0,0 +1,5 @@
+L = [1, 20, 300, 5, 44, 32]
+string = []
+for i in L:
+    string.append(str(i).zfill(3))
+print(string)
